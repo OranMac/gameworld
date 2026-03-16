e11000352c73a58f68522f1a7d3371f2820300ef
Oran Mac Carthy Halpin <oran.mac.carthy.halpin@ibm.com>
2026-03-16 15:44:45 +0000

    Implement emotional damage combat system

--- src/entities/entity.rs
+++ src/entities/entity.rs
@@ -2,29 +2,40 @@ use crate::systems::movement::MovementStrategy;
 use crate::systems::combat::CombatStrategy;
 use crate::factories::entity_factory::EntityFactory;
 
+// Emotion target is public due to enums being private by default
+#[derive(PartialEq)]
+pub enum EmotionTarget {
+    Pride,
+    Ego,
+    Greed,
+}
+
 pub struct Entity {
     pub name: String,
     pub position: (f32, f32),
+    pub emo_weakness: EmotionTarget,
     pub movement: Box<dyn MovementStrategy>,
     pub combat: Box<dyn CombatStrategy>,
 }
 
 impl Entity {
     // Create entity via Abstract (Entity)Factory
-    pub fn new(name: &str, factory: &dyn EntityFactory) -> Self {
+    pub fn new(name: &str, emo_target: EmotionTarget, factory: &dyn EntityFactory) -> Self {
         Self {
             name: name.to_string(),
             position: (0.0,0.0),
+            emo_weakness: emo_target,
             movement: factory.create_movement(),
             combat: factory.create_combat(),
         }
     }
 
     // Create entity via menu-selected strategies
-    pub fn from_strategies(name: &str, movement: Box<dyn MovementStrategy>, combat: Box<dyn CombatStrategy>) -> Self {
+    pub fn from_strategies(name: &str, emo_target: EmotionTarget, movement: Box<dyn MovementStrategy>, combat: Box<dyn CombatStrategy>) -> Self {
         Self {
             name: name.to_string(),
             position: (0.0,0.0),
+            emo_weakness: emo_target,
             movement,
             combat,
         }
@@ -34,8 +45,8 @@ impl Entity {
         self.position = self.movement.compute_next_waypoint(self.position);
     }
 
-    pub fn attack(&self, attack_power: f32) -> f32 {
-        self.combat.calculate_attack_damage(attack_power)
+    pub fn attack(&self,oponent: &Entity, attack_power: f32) -> f32 {
+        self.combat.calculate_attack_damage(oponent,attack_power)
     }
 
     pub fn defend(&self, damage: f32) -> f32 {
--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -1,6 +1,9 @@
+use crate::entities::entity::Entity;
+use crate::EmotionTarget;
+
 pub trait CombatStrategy {
     // Compute attack damage
-    fn calculate_attack_damage(&self, attack_power: f32) -> f32;
+    fn calculate_attack_damage(&self, oponent: &Entity, attack_power: f32) -> f32;
     // Compute damage taken after defense
     fn calculate_damage_taken(&self, incoming_damage: f32) -> f32;
 }
@@ -21,12 +24,41 @@ impl AggressiveCombat {
 
 impl CombatStrategy for AggressiveCombat {
     // Calculate attack damage: multiply attack_power by damage_multiplier
-    fn calculate_attack_damage(&self, attack_power: f32) -> f32 {
+    fn calculate_attack_damage(&self, oponent: &Entity, attack_power: f32) -> f32 {
         attack_power * self.damage_multiplier
     }
 
     // Handle defense: aggressive strategy does not reduce incoming damage
     fn calculate_damage_taken(&self, incoming_damage: f32) -> f32 {
-        return incoming_damage;
+        incoming_damage
+    }
+}
+
+
+// Future Idea for enum bases emotional damage, but no target yet to really implement it
+
+#[derive(PartialEq)]
+pub struct EmotionalDamageCombat {
+    emotional_damage: EmotionTarget
+}
+
+impl EmotionalDamageCombat {
+    pub fn new(target: EmotionTarget) -> Self {
+        Self {
+            // Select the which part of ones person to target
+            emotional_damage: target,
+        }
+    }
+}
+
+impl CombatStrategy for EmotionalDamageCombat {
+    // Calculate attack damage: multiply attack_power by damage_multiplier
+    fn calculate_attack_damage(&self, oponent: &Entity, attack_power: f32) -> f32 {
+        attack_power * if self.emotional_damage == oponent.emo_weakness { 2.0 } else { 0.5 }
+    }
+
+    // Handle defense: aggressive strategy does not reduce incoming damage
+    fn calculate_damage_taken(&self, incoming_damage: f32) -> f32 {
+        incoming_damage
     }
 }
