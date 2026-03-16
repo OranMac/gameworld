9b914e543c37155579f2c3f63a3201f91ba86766
Oran Mac Carthy Halpin <oran.mac.carthy.halpin@ibm.com>
2026-03-16 14:44:00 +0000

    Remove redundant run

--- src/game.rs
+++ src/game.rs
@@ -5,29 +5,6 @@ use crate::entities::entity::Entity;
 pub struct Game;
 
 impl Game {
-
-    pub fn run(
-        mut movement_strategy: Box<dyn MovementStrategy>,
-        combat_strategy: Box<dyn CombatStrategy>,
-    ) {
-        
-        println!("\n--- Starting Movement Simulation ---");
-        let mut waypoint = (0.0, 0.0);
-        for i in 0..5 {
-            waypoint = movement_strategy.compute_next_waypoint(waypoint);
-            println!("Step {}: Moved to position {:?}", i + 1, waypoint);
-        }
-        println!("------------------------------------\n");
-
-        let attack_power = 10.0;  // attack strength
-        let damage_dealt = combat_strategy.calculate_attack_damage(attack_power);
-        let damage_received = combat_strategy.calculate_damage_taken(5.0);
-        println!(
-            "Combat Simulation: base attack strength {} -> damage dealt {} | damage received {}",
-            attack_power, damage_dealt, damage_received
-        );
-
-    }
         // Run movement and combat for two entities (player and enemy)
     pub fn run_entity(player: &mut Entity, enemy: &mut Entity) {
         
