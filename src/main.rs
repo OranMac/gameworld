b794c2f8efc771362bc9348255c6827e8cd71989
Oran Mac Carthy Halpin <oran.mac.carthy.halpin@ibm.com>
2026-03-16 12:54:14 +0000

    Implement initail patrol movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+pub mod systems;
+
+use systems::movement::{MovementStrategy, PatrolMovement};
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("\n--- Starting Movement Simulation ---");
+
+    // Define the patrol route i.e. three waypoints to patrol here.
+    let mut patrol_route = PatrolMovement::new(vec![(0.0, 0.0), (5.0, 0.0), (5.0, 5.0)]);
+
+    // Start at (0.0, 0.0) and move between the defined patrol waypoints
+    let mut current_waypoint = (0.0, 0.0);
+    for _ in 0..5 {
+        current_waypoint = patrol_route.compute_next_waypoint(current_waypoint);
+        println!("Moved to waypoint: {:?}", current_waypoint);
+    }
+
+    println!("------------------------------------\n");
+
 }
---
+++ src/systems/mod.rs
@@ -0,0 +1,2 @@
+// Exposes the `movement` module as part of the `systems` namespace.
+pub mod movement;
